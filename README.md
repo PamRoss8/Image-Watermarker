5a797d9205738433511db210752b6d00227016c4
PamRoss8 <stepnzorskij@gmail.com>
2025-02-09 23:56:24 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Image-Watermarker
+# Image Watermarker
+
+This is a Python project for image watermarker. More details will be added soon.
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Image Watermarker
+This is the main script for the image watermarker project.
+"""
+
+def main():
+    print("Welcome to Image Watermarker!")
+
+if __name__ == "__main__":
+    main()
